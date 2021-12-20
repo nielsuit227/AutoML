78b44a07cacf08afca93eef4e36f71dbfa94b4fe
Niels Uitterdijk <niels.uit@gmail.com>
2021-12-20 11:14:21 +0100

    test get_required()

--- tests/Unit/AutoML/test_feature_processing.py
+++ tests/Unit/AutoML/test_feature_processing.py
@@ -97,3 +97,11 @@ class TestDataProcessing(unittest.TestCase):
             assert len(v) == len(xtn.keys()), "Incorrect number of keys"
             assert all(xt[v].keys() == xtn.keys()), 'Keys are not correct'
             assert np.allclose(xt[v], xtn), 'Transformed data not consistent for {} set'.format(k)
+
+    def test_get_required(self):
+    y = pd.Series(np.linspace(2, 100, 100))
+    b = pd.Series(np.linspace(-4, 4, 100) ** 2)
+    x = pd.DataFrame({'a': y / b, 'b': b, 'c': b / 2})
+    fp = FeatureProcesser(mode='regression')
+    xt, sets = fp.fit_transform(x, y)
+    assert fp.get_required_features(['a__x__b']) == list('b', 'a')
