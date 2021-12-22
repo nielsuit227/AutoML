7942ef8df0478b7fdfba135672ab6be481dbfb31
Niels Uitterdijk <niels.uit@gmail.com>
2021-12-22 17:08:08 +0100

    test add bins & categorical

--- tests/Unit/AutoML/test_drift_detector.py
+++ tests/Unit/AutoML/test_drift_detector.py
@@ -28,3 +28,18 @@ class TestDriftDetector(unittest.TestCase):
         assert len(drift.check(test)) == 0, "Test data found inconsistent"
         assert len(drift.check(ref.max() + 1)) == len(dists), "Maxima not detected"
         assert len(drift.check(ref.min() - 1)) == len(dists), "Minima not detected"
+
+    def test_categorical(self):
+        df = pd.DataFrame({'a': ['a', 'b', 'c', 'd', 'a', 'b', 'a', 'b', 'c', 'a']})
+        drift = DriftDetector(cat_cols=['a'])
+        drift.fit(df)
+        assert 'a' in drift.bins, "Column 'a' rejected."
+        assert drift.bins['a'] == {'a': 4, 'b': 3, 'c': 2, 'd': 1}
+
+    def test_add_bins(self):
+        df = pd.DataFrame({'a': ['a', 'b', 'c', 'd', 'a', 'b', 'a', 'b', 'c', 'a']})
+        drift = DriftDetector(cat_cols=['a'])
+        drift.fit(df)
+        new_bins = drift.add_bins(drift.bins, df)
+        assert new_bins['a'] == {'a': 8, 'b': 6, 'c': 4, 'd': 2}
+
