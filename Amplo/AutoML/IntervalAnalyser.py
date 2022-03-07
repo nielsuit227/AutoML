50ff5619830d6907d39f58273cf082ff3db076b1
Niels Uitterdijk <niels.uit@gmail.com>
2022-03-07 14:45:52 +0100

    annoy > faiss

--- Amplo/AutoML/IntervalAnalyser.py
+++ Amplo/AutoML/IntervalAnalyser.py
@@ -1,8 +1,9 @@
 import os
+import faiss
 import numpy as np
 import pandas as pd
-from annoy import AnnoyIndex
 from Amplo.AutoML import DataProcesser
+from Amplo.AutoML import FeatureProcesser
 
 
 class IntervalAnalyser:
@@ -10,22 +11,21 @@ class IntervalAnalyser:
     def __init__(self,
                  folder: str = None,
                  norm: str = 'euclidean',
+                 min_length: int = 1000,
                  n_neighbors: int = None,
                  n_trees: int = 10,
+                 verbose: int = 1,
                  ):
         """
-        Interval Analyser for Log file classification. When log files have to be classifier, and there is not enough
-        data for time series methods (such as LSTMs, ROCKET or Weasel, Boss, etc), one needs to fall back to classical
-        machine learning models which work better with lower samples. But this raises the problem of which samples to
-        classify. You shouldn't just simply classify on every sample and accumulate, that may greatly disrupt
-        classification performance. Therefore, we introduce this interval analyser. By using K-Nearest Neighbors,
-        one can estimate the strength of correlation for every sample inside a log. Using this allows for better
-        interval selection for classical machine learning models.
+        Interval Analyser for Log file classification. Has two purposes:
+        - Remove healthy data in longer, faulty logs
+        - Remove redundant data in large datasets
 
-        To use this interval analyser, make sure:
-        - That your logs are of equal length
-        - That your logs have identical keys
-        - That your logs are located in a folder of their class, with one parent folder with all classes, e.g.:
+        Uses Facebook's FAISS for K-Nearest Neighbors approximation.
+
+        ** IMPORTANT **
+        To use this interval analyser, make sure that your logs are located in a folder of their class, with one parent
+        folder with all classes, e.g.:
         +-- Parent Folder
         |   +-- Class_1
         |       +-- Log_1.*
@@ -35,29 +35,38 @@ class IntervalAnalyser:
 
         Parameters
         ----------
-        folder [str]:         Parent folder of classes
-        index_col [str]:      For reading the log files
-        norm [str]:           Optimization metric for K-Nearest Neighbors
-        n_neighbors [int]:    Quantity of neighbors, default to 3 * log length
-        n_trees [int]:        Quantity of trees
+        folder [str]:       Parent folder of classes
+        index_col [str]:    For reading the log files
+        min_length [int]:   Minimum length to cut off, everything shorter is left untouched
+        norm [str]:         Optimization metric for K-Nearest Neighbors
+        n_neighbors [int]:  Quantity of neighbors, default to 3 * log length
+        n_trees [int]:      Quantity of trees
         """
         # Parameters
         self.folder = folder + '/' if len(folder) == 0 or folder[-1] != '/' else folder
+        self.min_length = min_length
         self.norm = norm
         self.n_trees = n_trees
-        self.n_samples, self.n_keys = self._check_length()
-        self.n_neighbors = 3 * self.n_samples if n_neighbors is None else n_neighbors
+        self.n_neighbors = n_neighbors
+        self.verbose = verbose
 
         # Initializers
+        self.keys = None
+        self.n_keys = None
+        self.samples = None
+        self.avg_samples = None
         self.n_files = 0
         self.n_folders = 0
-        self.means = None
-        self.stds = None
+        self._mins = pd.DataFrame(index=[0])
+        self._maxs = pd.DataFrame(index=[0])
+        self._labels = None
+        self._engine = None
+        self._distributions = None
 
         # Test
         assert norm in ['euclidean', 'manhattan', 'angular', 'hamming', 'dot']
 
-    def analyse(self) -> np.ndarray:
+    def fit_transform(self) -> pd.DataFrame:
         """
         Function that runs the K-Nearest Neighbors and returns a NumPy array with the sensitivities.
 
@@ -65,41 +74,142 @@ class IntervalAnalyser:
         -------
         np.array: Estimation of strength of correlation
         """
-        # Set up
-        engine = AnnoyIndex(self.n_keys, self.norm)
-
         # Get data
         df, labels = self._parse_data()
 
-        # Index
-        for i, row in df.iterrows():
-            engine.add_item(i[0] * self.n_samples + i[1], row.values)
+        # Set up Annoy Engine (only now that n_keys is known)
+        self._engine = self._build_engine(df)
 
-        # Build
-        engine.build(self.n_trees)
+        # Make distribution
+        self._distributions = self._make_distribution(df, labels)
 
-        # Return distribution
-        return self._make_distribution(engine, df, labels)
+        # Return new dataset
+        return self._create_dataset(df, labels)
 
-    def save(self, path):
-        pass
-
-    def load(self, path):
-        pass
-
-    def _check_length(self) -> [int, int]:
+    def _create_dataset(self, df: pd.DataFrame, labels: pd.Series) -> pd.DataFrame:
         """
-        Detects the extension from the log files
+        This function selects samples given the calculated distributions. It only removes samples from logs which are
+        longer (> min_length), and only the samples with lower in-class neighbors.
+
+        One could come up with a fancier logic, using the total dataset samples, the class-balance & sample redundancy.
+
+        parameters
+        ----------
         """
-        # Read first log
-        folder = os.listdir(self.folder)[0]
-        file = os.listdir(self.folder + folder)[0]
-        df = self._read(f"{self.folder}{folder}/{file}")
+        # Verbose
+        if self.verbose > 0:
+            print('[AutoML] Creating filtered dataset')
 
-        # Set lengths
-        length, keys = len(df), len(df.keys())
+        # Get in-class means and number of samples for each label
+        # label_means = self._get_label_means(labels, self._distributions)
+        # label_samples = labels.value_counts()
 
-        return length, keys
+        # Iterate through labels and see if we should remove values
+        for i in range(self.n_files):
+
+            # Check length and continue if short
+            if sum(df.index.get_level_values(0) == i) < self.min_length:
+                continue
+
+            # Check distribution and find cut-off
+            dist = self._distributions[i]
+            ind_remove = [(i, j) for j in np.where(dist < dist.mean())[0]]
+
+            # Verbose
+            if len(ind_remove) > 0 and self.verbose > 1:
+                print(f'[AutoML] Removing {len(ind_remove)} samples from {labels[(i, 0)]}/Zie Gy')
+
+            # Remove samples from df
+            df = df.drop(ind_remove, axis=0)
+
+        return df
+
+    def _parse_data(self):
+        """
+        Loops through all files, cleans them and notes down sizes.
+        """
+        """
+        Reads all files and sets a multi index
+        Returns
+        -------
+        pd.DataFrame: all log files
+        """
+        # Verbose
+        if self.verbose > 0:
+            print('[AutoML] Parsing data for interval analyser.')
+
+        # Result init
+        dfs = []
+
+        # Loop through files
+        for folder in os.listdir(self.folder):
+            for file in os.listdir(self.folder + folder):
+
+                # Verbose
+                if self.verbose > 1:
+                    print(f"[AutoML] {self.folder}{folder}/{file}")
+
+                # Read df
+                df = self._read(f'{self.folder}{folder}/{file}')
+
+                # Set label
+                df['class'] = folder
+
+                # Set second index
+                df = df.set_index(pd.MultiIndex.from_product([[self.n_files], df.index.values], names=['log', 'index']))
+
+                # Add to list
+                dfs.append(df)
+
+                # Increment
+                self.n_files += 1
+            self.n_folders += 1
+
+        # Concatenate dataframes
+        dfs = pd.concat(dfs)
+
+        # Remove classes
+        labels = dfs['class']
+        dfs = dfs.drop('class', axis=1)
+
+        # Clean data
+        dp = DataProcesser(missing_values='zero')
+        dfs = dp.fit_transform(dfs)
+
+        # Remove datetime columns
+        if len(dp.date_cols) != 0:
+            dfs = dfs.drop(dp.date_cols, axis=1)
+
+        # Select features
+        fp = FeatureProcesser(extract_features=False)
+        dfs, sets = fp.fit_transform(dfs, labels)
+        dfs = dfs[sets['ShapThreshold']]
+
+        # Normalize
+        dfs = (dfs - dfs.min()) / (dfs.max() - dfs.min())
+
+        # Set sizes
+        self.samples = len(dfs)
+        self.n_keys = len(dfs.keys())
+        if self.n_neighbors is None:
+            self.n_neighbors = int(3 * self.samples / self.n_files)
+
+        # Return
+        return dfs, labels
+
+    def _build_engine(self, df):
+        """
+        Builds the ANNOY engine.
+        """
+        # Create engine
+        if self.verbose > 0:
+            print('[AutoML] Building interval analyser engine.')
+        engine = faiss.IndexFlatL2(self.n_keys)
+
+        # Add the data to ANNOY
+        engine.add(np.ascontiguousarray(df.values))
+
+        return engine
 
     def _read(self, path: str) -> pd.DataFrame:
         """
@@ -123,73 +233,35 @@ class IntervalAnalyser:
         else:
             raise NotImplementedError('File format not supported.')
 
-    def _parse_data(self) -> [pd.DataFrame, pd.Series]:
-        """
-        Reads all files and sets a multi index
-        Returns
-        -------
-        pd.DataFrame: all log files
-        """
-        # Result init
-        dfs = []
-
-        # Loop through files
-        for folder in os.listdir(self.folder):
-            for file in os.listdir(self.folder + folder):
-
-                # Read df
-                df = self._read(f'{self.folder}{folder}/{file}')
-
-                # Set label
-                df['class'] = folder
-
-                # Set second index
-                df = df.set_index(pd.MultiIndex.from_product([[self.n_files], df.index.values], names=['log', 'index']))
-
-                # Add to list
-                dfs.append(df)
-
-                # Increment
-                self.n_files += 1
-            self.n_folders += 1
-
-        # Concatenate dataframes
-        dfs = pd.concat(dfs)
-
-        # Remove classes
-        classes = dfs['class']
-        dfs = dfs.drop('class', axis=1)
-        dp = DataProcesser(missing_values='zero')
-        dfs = dp.fit_transform(dfs)
-
-        # Remove datetime columns
-        if len(dp.date_cols) != 0:
-            dfs = dfs.drop(dp.date_cols, axis=1)
-
-        # Return
-        return dfs, classes
-
-    def _make_distribution(self, engine, df: pd.DataFrame, classes: pd.Series) -> np.ndarray:
+    def _make_distribution(self, df: pd.DataFrame, labels: pd.Series) -> pd.Series:
         """
         Given a build K-Nearest Neighbors, returns the label distribution
         """
-        # Result setup
-        distribution = [[] for _ in range(self.n_files)]
+        if self.verbose > 0:
+            print('[AutoML] Calculating interval within-class distributions.')
 
-        # Iterate through samples
-        for (log, sample), data in df.iterrows():
-            # Get neighbor indices
-            neighbor_inds = engine.get_nns_by_vector(data.values, self.n_neighbors)
+        # Search nearest neighbors for all samples
+        _, neighbors = self._engine.search(np.ascontiguousarray(df.values), self.n_neighbors)
 
-            # Set percentage
-            distribution[log].append((classes.iloc[neighbor_inds] == classes[(log, sample)]).sum() / self.n_neighbors)
+        # And calculate percentage in label
+        distribution = [sum(labels.iloc[n] == labels.iloc[i]) / self.n_neighbors for i, n in enumerate(neighbors)]
 
-        # Mask
-        lens = np.array([len(item) for item in distribution])
-        mask = lens[:, None] > np.arange(lens.max())
-        out = np.full(mask.shape, 0)
-        out[mask] = np.concatenate(distribution)
-        distribution = np.array(out)
+        # Parse into list of lists
+        return pd.Series(distribution, index=labels.index)
 
-        # Average over logs
-        return np.mean(distribution * 100, axis=0)
+    @staticmethod
+    def _get_label_means(labels, dists):
+        # Init
+        label_means = {k: [] for k in labels.unique()}
+
+        # Append distributions
+        for i, d in enumerate(dists):
+            # Skip failed reads
+            if i not in labels:
+                continue
+
+            # Extend distribution
+            label_means[labels[(i, 0)]].extend(d)
+
+        # Return means
+        return {k: np.mean(v) for k, v in label_means.items()}
