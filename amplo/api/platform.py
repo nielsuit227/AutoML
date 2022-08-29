b3d5901e52b9566a805b779262de1a37dd7d1055
Timo Streule <48442147+tstreule@users.noreply.github.com>
2022-08-29 15:41:17 +0200

    PLT-460 api debugging (#72)
    * Better error reporting
    
    * `PUT /trainings`, not `/models`
    
    * Add model id for upload

--- amplo/api/platform.py
+++ amplo/api/platform.py
@@ -8,6 +8,8 @@ from pathlib import Path
 
 import requests
 
+from amplo.utils import check_dtypes
+
 __all__ = ["PlatformSynchronizer"]
 
 
@@ -31,7 +33,7 @@ class PlatformSynchronizer:
         self.api_key = os.getenv(connection_string_name)
         self.verbose = int(verbose)
 
-    def upload_model(self, issue_dir, team, machine, service, issue, version):
+    def upload_model(self, issue_dir, team, machine, service, issue, version, model_id):
         """
         Upload trained model
 
@@ -49,6 +51,8 @@ class PlatformSynchronizer:
             Issue name
         version : int
             Version of production model to upload
+        model_id : int
+            Model identifier for platform.
 
         Notes
         -----
@@ -58,20 +62,26 @@ class PlatformSynchronizer:
         """
 
         # Check input args
-        assert os.path.isdir(issue_dir), "Model directory does not exist"
-        assert all(
-            isinstance(arg, str) for arg in (team, machine, service, issue)
-        ), "Invalid argument"
-        assert isinstance(version, int), "Invalid argument"
+        if not os.path.isdir(issue_dir):
+            raise FileNotFoundError("Model directory does not exist.")
+        check_dtypes(
+            ("team", team, str),
+            ("machine", machine, str),
+            ("service", service, str),
+            ("issue", issue, str),
+            ("version", version, int),
+        )
 
         # Check file structure
         file_dir = Path(issue_dir) / "Production" / f"v{version}"
-        assert os.path.isdir(
-            file_dir
-        ), f"Production files for version {version} do not exist"
+        if not os.path.isdir(file_dir):
+            raise FileNotFoundError(
+                f"Production files for version {version} don't exist."
+            )
         files = [file_dir / file for file in ("Settings.json", "Model.joblib")]
         for file in files:
-            assert file.exists(), f"File {file.name} does not exist"
+            if not file.exists():
+                raise FileNotFoundError(f"File {file} doesn't exist in {file_dir}.")
 
         # Set info
         headers = {"X-Api-Key": self.api_key}
@@ -90,7 +100,8 @@ class PlatformSynchronizer:
             get_response = requests.get(
                 self.api_url + "/models", params=data, headers=headers
             )
-            assert get_response.status_code == 200, "GET request has failed"
+            if get_response.status_code != 200:
+                raise requests.HTTPError(f"{get_response} {get_response.text}")
 
             # Get all existing models
             try:
@@ -102,8 +113,13 @@ class PlatformSynchronizer:
                 warnings.warn(f"Could not decode GET response - {err}")
                 # Set default value of ``existing_models`` to list of (empty) dict
                 existing_models = list(dict())
-            assert isinstance(existing_models, list), "Received unexpected data type"
-            assert isinstance(existing_models[0], dict), "Received unexpected data type"
+
+            # Check existing models
+            if not isinstance(existing_models, list):
+                raise ValueError(f"Unexpected data type: {type(existing_models)}")
+            for model in existing_models:
+                if not isinstance(model, dict):
+                    raise ValueError(f"Unexpected data type: {type(model)}")
 
             # Check if model exists
             return any(
@@ -120,17 +136,17 @@ class PlatformSynchronizer:
                 self.api_url + "/models", data=data, headers=headers
             )
             # Assert that now model exists
-            assert model_exists(
-                data["name"]
-            ), f"Failed to create model - {post_response} {post_response.text}"
+            if not model_exists(data["name"]):
+                raise requests.HTTPError(f"{post_response} {post_response.text}")
 
-        # Add files to data
-        data.update({"files": [open(file, "r") for file in files]})
+        # Add files and model id to data
+        data.update({"files": [open(file, "r") for file in files], "id": model_id})
         # PUT a new version
         put_response = requests.put(
-            self.api_url + "/models", data=data, headers=headers
+            self.api_url + "/trainings", data=data, headers=headers
         )
-        assert put_response.status_code == 200
+        if put_response.status_code != 200:
+            raise requests.HTTPError(f"{put_response} {put_response.text}")
 
     def upload_latest_model(self, issue_dir, *args, **kwargs):
         """
